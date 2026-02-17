2fbaa09402e4b960563b3e5f8c2c9aac5c5944f3
Kotmin <70173732+Kotmin@users.noreply.github.com>
2026-02-17 13:19:42 +0100

    fix: rm old text version

--- test/test_output_file_txt.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-def test_tstring_comparsion():
-        with open('Kto_ma_co.txt') as f:
-            to_cmp=f.read()
-        assert to_cmp.strip()=="Ala ma kota od 2024-10-31"
-            
-       
